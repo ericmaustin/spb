979c49d2fc3cfe2cebf0cc91fe73441bc7c26178
Eric Austin <eric.m.austin@gmail.com>
2019-03-30 13:31:26 -0500

    minor tweaks

--- main.go
+++ main.go
@@ -21,14 +21,14 @@ var (
 
 	Text = `**If you're in crises, please consider contacting the [National Suicide Prevention Lifeline](https://suicidepreventionlifeline.org/): 1-800-273-TALK**
 
-	If you wish to help others in crises consider supporting:
-	- [The National Suicide Prevention Lifeline](https://suicidepreventionlifeline.org/donate/)
-	- [The National Alliance on Mental Illness](https://donate.nami.org/give/197406/#!/donation/checkout)
-	- [The Jason Foundation](http://jasonfoundation.com/get-involved/)
+If you wish to help others in crises consider supporting:
+- [The National Suicide Prevention Lifeline](https://suicidepreventionlifeline.org/donate/)
+- [The National Alliance on Mental Illness](https://donate.nami.org/give/197406/#!/donation/checkout)
+- [The Jason Foundation](http://jasonfoundation.com/get-involved/)
 
-		
-	I am a bot created by a survivor that is in no way affiliated with any of the organizations mentioned | [feedback](mailto:erics.awesome.bots@gmail.com)
-	`
+	
+I am a bot created by a survivor that is in no way affiliated with any of the organizations mentioned | [feedback](mailto:erics.awesome.bots@gmail.com)
+`
 
 	matchExpressions = []string{
 		`i\s*(am\sgoing\sto|will)\skill\smyself`,
